649e84073588002684d59ebee2f88a2b2c4849ee
Bagasbelajarhtml <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-08-16 20:35:02 +0700

    add migration

---
+++ database/migrations/2025_08_16_121908_create_jawabans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jawabans', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('pengguna_id')->nullable();
+            $table->unsignedBigInteger('gejala_id')->nullable();
+            $table->unsignedBigInteger('adiksi_id')->nullable();
+            $table->float('cf_user');
+            $table->float('cf_kombinasi');
+            $table->foreign('pengguna_id')->references('id')->on('penggunas');
+            $table->foreign('gejala_id')->references('id')->on('gejalas');
+            $table->foreign('adiksi_id')->references('id')->on('lvl_adiksis');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jawabans');
+    }
+};
