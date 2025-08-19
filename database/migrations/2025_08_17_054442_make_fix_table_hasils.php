9ea36eb204c431c117d940a5317d028d8b5b7860
Bagasbelajarhtml <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-08-19 14:51:12 +0700

    done

--- database/migrations/2025_08_17_054442_make_fix_table_hasils.php
+++ database/migrations/2025_08_17_054442_make_fix_table_hasils.php
@@ -13,21 +13,18 @@ return new class extends Migration
     {
         Schema::table('hasils', function (Blueprint $table) {
             $table->dropColumn([
-                // 'pengguna',
-                // 'cf_craving',
-                // 'cf_tolerance',
-                // 'cf_withdrawal',
-                // 'cf_preoccupation_salience',
-                // 'cf_continued_use_despite_harm',
-                // 'cf_functional_impairment'
+                'cf_loss_of_control',
+                'cf_craving',
+                'cf_tolerance',
+                'cf_withdrawal',
+                'cf_preoccupation_salience',
+                'cf_continued_use_despite_harm',
+                'cf_functional_impairment',
             ]);
-            // $table->unsignedBigInteger('adiksi_id');
-            // $table->unsignedBigInteger('pengguna_id');
-            // $table->float('cf_final');
-            // $table->foreign('adiksi_id')->references('id')->on('lvl_adiksis');
-            // $table->foreign('pengguna_id')->references('id')->on('penggunas');
-            // $table->date('tgl_diagnosa')->useCurrent();
-            $table->timestamps();
+            $table->unsignedBigInteger('adiksi_id');
+            $table->float('cf_final');
+            $table->foreign('adiksi_id')->references('id')->on('lvl_adiksis');
+            $table->date('tgl_diagnosa')->useCurrent();
         });
     }
 
@@ -36,14 +33,21 @@ return new class extends Migration
      */
     public function down(): void
     {
-        // Schema::table('hasils', function (Blueprint $table) {
-        //     $table->float('cf_loss_of_control')->after('pengguna_id');
-        //     $table->float('cf_craving')->after('cf_loss_of_control');
-        //     $table->float('cf_tolerance')->after('cf_craving');
-        //     $table->float('cf_withdrawal')->after('cf_tolerance');
-        //     $table->float('cf_preoccupation_salience')->after('cf_withdrawal'); // ubah dari /
-        //     $table->float('cf_continued_use_despite_harm')->after('cf_preoccupation_salience');
-        //     $table->float('cf_functional_impairment')->after('cf_continued_use_despite_harm');
-        // });
+        Schema::table('hasils', function (Blueprint $table) {
+            $table->float('cf_loss_of_control')->after('pengguna_id');
+            $table->float('cf_craving')->after('cf_loss_of_control');
+            $table->float('cf_tolerance')->after('cf_craving');
+            $table->float('cf_withdrawal')->after('cf_tolerance');
+            $table->float('cf_preoccupation_salience')->after('cf_withdrawal'); // ubah dari /
+            $table->float('cf_continued_use_despite_harm')->after('cf_preoccupation_salience');
+            $table->float('cf_functional_impairment')->after('cf_continued_use_despite_harm');
+            //
+            $table->dropForeign(['adiksi_id']);
+            $table->dropColumn([
+                'adiksi_id',
+                'cf_final',
+                'tgl_diagnosa',
+            ]);
+        });
     }
 };
